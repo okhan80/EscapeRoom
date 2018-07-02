117af421c44f6588b88ad3c5b96657879b387aaa
Omar Khan <okhan80@gmail.com>
2018-07-02 15:10:49 -0700

    Get player pawn during gameplay for trigger

--- Source/EscapeRoom/OpenDoor.cpp
+++ Source/EscapeRoom/OpenDoor.cpp
@@ -2,6 +2,7 @@
 
 #include "OpenDoor.h"
 #include "GameFramework/Actor.h"
+#include "Engine/World.h"
 
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
@@ -26,7 +27,9 @@ void UOpenDoor::OpenDoor()
 // Called when the game starts
 void UOpenDoor::BeginPlay()
 {
-	Super::BeginPlay();	
+	Super::BeginPlay();
+
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
 }
 
 
